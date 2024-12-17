51d1f66c187785c74bd152bdcdd9d46facb9087b
leejoonwoo <leejoonwoo.kr@gmail.com>
2024-12-17 18:19:45 +0900

    node - chat

---
+++ chat/schema/room.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const roomSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    ref: "Room",
+  },
+  max: {
+    type: Number,
+    required: true,
+    default: 10,
+    min: 2,
+  },
+  owner: {
+    type: String,
+    required: true,
+  },
+  password: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Room", roomSchema);
